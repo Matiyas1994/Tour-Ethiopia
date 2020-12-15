e93d76e5949415a337ba222bc19ae596b914d58b
Matiyas1994 <matiyasy1@gmail.com>
2020-12-15 17:38:11 +0300

    adding tab and app bar to the app adding fragments

--- app/src/main/java/com/example/tourethiopia/MainActivity.java
+++ app/src/main/java/com/example/tourethiopia/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.tourethiopia;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,5 +17,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarid);
         setSupportActionBar(toolbar);
+
+
+        SectionsPagerAdapter pagerAdapter= new SectionsPagerAdapter(getSupportFragmentManager());
+        android.support.v4.view.ViewPager viewPager=(ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(pagerAdapter);
+
+        //Attach the ViewPager to the TabLayout
+        android.support.design.widget.TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
+
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            switch (i){
+                case 0:
+                    return new PlaceFragment();
+                case 1:
+                    return new HotelFragment();
+                case 2:
+                    return new TravelagenciesFragment();
+                case 3:
+                    return new MapFragment();
+            }
+            return null;
+        }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return getResources().getText(R.string.place_tab);
+                case 1:
+                    return getResources().getText(R.string.hotel_tab);
+                case 2:
+                    return getResources().getText(R.string.travelagencies_tab);
+                case 3:
+                    return getResources().getText(R.string.map_tab);
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+    }
+
 }
