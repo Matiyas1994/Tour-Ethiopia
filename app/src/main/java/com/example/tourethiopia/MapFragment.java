1c6fe51dfbf392e6d7186c83e8c671856206ff8f
Matiyas1994 <matiyasy1@gmail.com>
2020-12-16 18:31:27 +0300

    Adding google map to Map tab

--- app/src/main/java/com/example/tourethiopia/MapFragment.java
+++ app/src/main/java/com/example/tourethiopia/MapFragment.java
@@ -1,19 +1,48 @@
 package com.example.tourethiopia;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 
 public class MapFragment extends Fragment {
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+
+        View myview= inflater.inflate(R.layout.fragment_map, container, false);
+
+        final ProgressBar progressBar=myview.findViewById(R.id.progressbar);
+        WebView mapwebview= myview.findViewById(R.id.mapview);
+        WebSettings webSettings =mapwebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+mapwebview.setWebViewClient(new WebViewClient(){
+
+    @Override
+    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        super.onPageStarted(view, url, favicon);
+progressBar.setVisibility(view.VISIBLE);
+    }
+
+    @Override
+    public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
+        progressBar.setVisibility(view.GONE);
+    }
+});
+
+        mapwebview.loadUrl("https://www.google.com/maps");
+
+        return myview;
+
     }
 }
